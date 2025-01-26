2c982fa76f76deba1c55bcb24f5d197a73ee58dd
AI-rain-code <airin.akther2000@gmail.com>
2025-01-26 15:33:07 +0600

    PawsitiveWellbeing

--- Main/control/RegistrationRedirectControl.php
+++ Main/control/RegistrationRedirectControl.php
@@ -4,26 +4,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve the selected category
     $category = $_POST['category'] ?? '';
 
-    // Define the base directory
-    $baseDir = '/PawsitiveWellbeing';
 
     // Redirect based on the category
     switch ($category) {
         case 'GeneralUser':
-            header("Location: {$baseDir}/General User/view/GeneralUserRegistration.php");
+            header("Location: '/PawsitiveWellbeing/General User/view/GeneralUserRegistration.php");
             break;
         case 'Volunteer':
-            header("Location: {$baseDir}/Volunteer/view/VolunteerRegistration.php");
+            header("Location: '/PawsitiveWellbeing/Volunteer/view/VolunteerRegistration.php");
             break;
         case 'Benefactor':
-            header("Location: {$baseDir}/Benefactor/view/BenefactorRegistration.php");
+            header("Location: '/PawsitiveWellbeing/Benefactor/view/BenefactorRegistration.php");
             break;
         case 'Veterinarian':
-            header("Location: {$baseDir}/Veterinarian/view/VeterinarianRegistration.php");
+            header("Location: '/PawsitiveWellbeing/Veterinarian/view/VeterinarianRegistration.php");
             break;
         default:
             // Redirect to an error page or the home page if the category is invalid
-            header("Location: {$baseDir}/Main/view/Error.php");
+            header("Location: '/PawsitiveWellbeing/Main/view/Error.php");
             break;
     }
     exit();
