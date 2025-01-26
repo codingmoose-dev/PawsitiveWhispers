d710daea603094b6e6842b5e6f28cfe56fc8ec74
Mohammed Mostafa <thecodingmoose.official@outlook.com>
2025-01-26 15:50:13 +0600

    PawsitiveWellbeing

--- Main/control/RegistrationRedirectControl.php
+++ Main/control/RegistrationRedirectControl.php
@@ -7,25 +7,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Redirect based on the category
     switch ($category) {
         case 'GeneralUser':
-            header("Location: '/PawsitiveWellbeing/General User/view/GeneralUserRegistration.php");
+            header("Location: /PawsitiveWellbeing/GeneralUser/view/GeneralUserRegistration.php");
             break;
         case 'Volunteer':
-            header("Location: '/PawsitiveWellbeing/Volunteer/view/VolunteerRegistration.php");
+            header("Location: /PawsitiveWellbeing/Volunteer/view/VolunteerRegistration.php");
             break;
         case 'Benefactor':
-            header("Location: '/PawsitiveWellbeing/Benefactor/view/BenefactorRegistration.php");
+            header("Location: /PawsitiveWellbeing/Benefactor/view/BenefactorRegistration.php");
             break;
         case 'Veterinarian':
-            header("Location: '/PawsitiveWellbeing/Veterinarian/view/VeterinarianRegistration.php");
+            header("Location: /PawsitiveWellbeing/Veterinarian/view/VeterinarianRegistration.php");
             break;
         default:
             // Redirect to an error page or the home page if the category is invalid
-            header("Location: '/PawsitiveWellbeing/Main/view/Error.php");
+            header("Location: /PawsitiveWellbeing/Main/view/Error.php");
             break;
     }
     exit();
 } else {
     // Redirect to the home page if the request method is not POST
-    header('Location: /PawsitiveWellbeing/index.php');
+    header("Location: /PawsitiveWellbeing/index.php");
     exit();
 }
